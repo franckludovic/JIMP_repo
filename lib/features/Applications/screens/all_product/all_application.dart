c0684bf31017d8c1a6b4b217c6b54db31f64f914
franckludovic <tankeundossefranck@gmail.com>
2025-02-17 18:47:37 +0100

    updates up to the application page

--- lib/features/Applications/screens/all_product/all_application.dart
+++ lib/features/Applications/screens/all_product/all_application.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:iconsax/iconsax.dart';
 import 'package:project_bc_tuto/common/widgets/appbar/appbar.dart';
-import 'package:project_bc_tuto/common/widgets/applications/applications_cards/applications_card_vertical.dart';
-import 'package:project_bc_tuto/common/widgets/layout/grid_layout.dart';
 import 'package:project_bc_tuto/utils/constants/sizes.dart';
+
+import '../../../../common/widgets/applications/sortable/sortable_application.dart';
 
 class AllApplications extends StatelessWidget {
   const AllApplications({super.key});
@@ -12,31 +11,16 @@ class AllApplications extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: JAppbar(
-        title: Text('Popular Applications'),
+        title: Text('Popular Applications',),
         showBackArrow: true,
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(JSizes.defaultSpace),
-          child: Column(
-            children: [
-              ///DropDown
-              DropdownButtonFormField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Iconsax.sort)
-                ),
-                onChanged: (value) {},
-                items: ['Name', 'Higher Price', 'Lower Price', 'Sale', 'Newest', 'Popularity'].map((option) => DropdownMenuItem(value:option, child: Text(option))).toList(),
-
-              ),
-
-              const SizedBox(height: JSizes.spaceBtwSections,),
-              /// applications
-              JGridLayout(itemCount: 16, itemBuilder: (_, index) => JApplicationsCardVertical())
-            ],
-          ),
+          child: JSortableApplications(),
         ),
       ),
     );
   }
 }
+
